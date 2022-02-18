9386c1f17ef1eca67de1cdb258dd057f5773fc09
David Wang <61897610+David-X-Wang@users.noreply.github.com>
2022-02-18 15:31:29 -0600

    Updated dependencies

--- Setup.m
+++ Setup.m
@@ -1,2 +1,10 @@
 % This file is for setup
-cerestim_depPath = './Cerestim_MNStim';
+
+
+cerestim_dep = './Cerestim_MNStim/Binaries.zip';
+cerestim_dep_path = './Cerestim_MNStim/';
+unzip(cerestim_dep,cerestim_dep_path)
+
+NPMK_dep = './NSP_Processing/NPMK.zip';
+NPMK_dep_path = './NSP_Processing/';
+unzip(NPMK_dep,NPMK_dep_path)
